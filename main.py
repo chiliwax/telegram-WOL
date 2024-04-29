53c4e98589326abed7d2a893e54761400440c689
Thibault Lecointe <thibault.lecointe@gimbalcube.com>
2024-04-29 17:40:34 +0200

    feat: test

--- main.py
+++ main.py
@@ -70,7 +70,8 @@ def getIpForMacAddr():
     responses, _ = srp(arp_request, timeout=2, verbose=False)
     # Iterate through responses
     for _, response in responses:
-        if response.hwsrc == PC_MAC_ADDR.replace(".", ":"):
+        logger.log(logging.INFO, response.hwsrc)
+        if response.hwsrc == PC_MAC_ADDR.replace(".", ":").lower():
             return response.psrc  # Return the IP address if MAC address matches
     return None  # Return None if MAC address not found
     
